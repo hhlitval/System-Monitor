5ea4e61f420ef980b0cae8e30d54891387682f2e
Sasha <sashajulkin@gmail.com>
2023-04-17 00:33:21 +0200

    Second try - progress bars added

--- System Monitor/MainWindow.xaml.cs
+++ System Monitor/MainWindow.xaml.cs
@@ -13,21 +13,24 @@ namespace System_Monitor
     {
         PerformanceCounter performanceCPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
         PerformanceCounter performanceRAM = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-        PerformanceCounter performancePageFile = new PerformanceCounter("Paging File", "% Usage", "_Total");
+        //PerformanceCounter performancePageFile = new PerformanceCounter("Paging File", "% Usage", "_Total");
         public MainWindow()
         {
             InitializeComponent();
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Interval = TimeSpan.FromSeconds(0.5);
             timer.Tick += timer_Tick;
             timer.Start();
         }
 
         void timer_Tick(object sender, EventArgs e)
-        {
-            labelCPU.Content = (int)performanceCPU.NextValue();
-            labelRAM.Content = (int)performanceRAM.NextValue();
-            labelPageFile.Content = (int)performancePageFile.NextValue();
+        {            
+            cpuProgressBar.Value = (double)performanceCPU.NextValue();
+            cpuLabel.Content = cpuProgressBar.Value;
+
+            ramProgressBar.Value = (double)performanceRAM.NextValue();
+            ramLabel.Content = (double)performanceRAM.NextValue();
+            //labelPageFile.Content = (double)performancePageFile.NextValue();
         }
     }
 }
