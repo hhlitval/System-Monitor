e843936bbbeb71dc89bb36a098a78e48f294bcd7
Sasha <sashajulkin@gmail.com>
2023-04-18 23:27:50 +0200

    Disk Usage and Network sections added

--- System Monitor/MainWindow.xaml.cs
+++ System Monitor/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Diagnostics;
 using System.Windows.Threading;
+using System.Windows.Input;
 
 namespace System_Monitor
 {
@@ -11,30 +12,45 @@ namespace System_Monitor
     /// </summary>
     public partial class MainWindow : Window
     {
-        PerformanceCounter performanceCPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        PerformanceCounter performanceCPU = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total");
+        PerformanceCounter timeCPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        PerformanceCounter osCPU = new PerformanceCounter("Processor", "% Privileged time", "_Total");
+        PerformanceCounter userCPU = new PerformanceCounter("Processor", "% User Time", "_Total");
+
         PerformanceCounter performanceRAM = new PerformanceCounter("Memory", "% Committed Bytes In Use");
-        //PerformanceCounter performancePageFile = new PerformanceCounter("Paging File", "% Usage", "_Total");
+        PerformanceCounter freeRAM = new PerformanceCounter("Memory", "Available MBytes");        
+
         public MainWindow()
         {
             InitializeComponent();
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += timer_Tick;
             timer.Start();
         }
 
         void timer_Tick(object? sender, EventArgs e)
-        {            
+        {
+            //CPU Usage
             cpuProgressBar.Value = (double)performanceCPU.NextValue();
             cpuLabel.Content = cpuProgressBar.Value;
-            cpuTimeLabel.Content = cpuProgressBar.Value; //This line defines the process time, not the above ones
-            osCpuUsageLabel.Content = cpuProgressBar.Value;
-            userCpuUsageLabel.Content = cpuProgressBar.Value;
+            cpuTimeLabel.Content = (double)timeCPU.NextValue(); //This line defines the process time, not the above ones
+            osCpuUsageLabel.Content = (double)osCPU.NextValue();
+            userCpuUsageLabel.Content = (double)userCPU.NextValue();               
 
+            //RAM Usage
             ramProgressBar.Value = (double)performanceRAM.NextValue();
-            ramLabel.Content = (double)performanceRAM.NextValue();
+            ramLabel.Content = ramProgressBar.Value;
             
             //labelPageFile.Content = (double)performancePageFile.NextValue();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
